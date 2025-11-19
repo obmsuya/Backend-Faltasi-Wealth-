6ae01344401ff749b0c7851cf0a2f3270fe3abd9
chrisostomemataba <doctorchrisostome@gmail.com>
2025-11-19 13:08:21 +0300

    feat: add faltasi-wealth.vercel.app to CORS origins

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ origins = [
     "http://localhost:3000", 
     "http://0.0.0.0:8000",
     "http://127.0.0.1:8000",
-    "https://faltasi.wapangaji.com"
+    "https://faltasi.wapangaji.com",
+    "https://faltasi-wealth.vercel.app"
 ]
 
 app.add_middleware(
